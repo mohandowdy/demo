57cc1034d6126a405061ff0661c494c6f37b0a30
test <test@pganjac.com>
2023-06-03 11:55:15 +0530

    FIRST

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./Welcome"
+
+const Home = () => {
+   return(
+      <>
+         <Welcome/>
+      </>
+   )
+}
+
+export default Home
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+   <>
+      <div className='d-flex justify-content-center align-items-center' style={{height: '100vh'}}>
+         <span className='h1'>Welcome</span>
+      </div>
+   </>
+  )
+}
+
+export default Welcome
